96084b92f2510d99e546200dfcb7a561d7a8b4ab
zwelhtetyan <zwelhtetyan2001@gmail.com>
2022-12-09 16:26:39 +0630

    selectable tag with keyboard arrow keys🫠🫠

--- app/frontend/src/pages/Create.jsx
+++ app/frontend/src/pages/Create.jsx
@@ -1,5 +1,4 @@
-import { useEffect } from 'react';
-import { useRef, useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
 import useClickOutside from '../../hooks/useClickOutside';
 import tagSuggestions from '../tagSuggestions.json';
 
@@ -8,23 +7,60 @@ const focusOutlineStyle =
 
 // Sgt => Suggestion;
 
-const SGT = ({ tagName, desc, onClick }) => (
-   <div
-      className='bg-cardBg hover:bg-tagBg py-2 px-3 cursor-pointer'
-      onClick={() => onClick(tagName)}
-   >
-      <h4 className='text-lg font-bold '>{tagName}</h4>
-      <p className='leading-5 text-sm'>{desc}</p>
-   </div>
-);
+const SGT = ({
+   tagName,
+   desc,
+   onClick,
+
+   idx,
+   currentIdx,
+   keyCode,
+}) => {
+   useEffect(() => {
+      if (currentIdx >= 0 && (keyCode === 40 || keyCode === 38)) {
+         const currentElem = document.getElementById(`tag-${currentIdx}`);
+
+         const observer = new IntersectionObserver(
+            (entries) => {
+               entries.forEach((entry) => {
+                  if (!entry.isIntersecting) {
+                     const option = keyCode === 40 ? false : true;
+                     currentElem.scrollIntoView(option);
+                  }
+               });
+            },
+            { threshold: 0.5 }
+         );
+
+         observer.observe(currentElem);
+
+         return () => observer.unobserve(currentElem);
+      }
+   }, [currentIdx]);
+
+   return (
+      <div
+         id={`tag-${idx}`}
+         className={`${
+            idx === currentIdx ? 'bg-tagBg' : 'bg-cardBg'
+         } hover:bg-tagBg py-2 px-3 cursor-pointer`}
+         onClick={() => onClick(tagName)}
+      >
+         <h4 className='text-lg font-bold '>{tagName}</h4>
+         <p className='leading-5 text-sm'>{desc}</p>
+      </div>
+   );
+};
 
 const Create = () => {
    // states
    const [suggestions, setSuggestions] = useState(tagSuggestions);
    const [availableTags, setAvailableTags] = useState(tagSuggestions);
    const [showSgtBox, setShowSgtBox] = useState(false);
-   const [sgtBoxTopPx, setSgtBoxTopPx] = useState(92);
+   const [sgtBoxTopPx, setSgtBoxTopPx] = useState(null);
    const [selectedTags, setSelectedTags] = useState([]);
+   const [currentIdx, setCurrentIdx] = useState(-1);
+   const [keyCode, setKeyCode] = useState(null);
 
    // refs
    const titleRef = useRef(null);
@@ -50,10 +86,6 @@ const Create = () => {
    const handleSelectTag = (tagName) => {
       setSelectedTags((prev) => [...prev, tagName]);
 
-      // remove selected tags from suggestions
-      setAvailableTags((prev) => prev.filter((t) => t.tagName !== tagName));
-      setSuggestions((prev) => prev.filter((t) => t.tagName !== tagName));
-
       handleClickSgtInput(); // refocus the next available input
       tagRef.current.value = '';
    };
@@ -61,15 +93,21 @@ const Create = () => {
    const handleSelectCustomTag = () => {
       const customTag = tagRef.current.value.trim();
 
-      const isDuplicateTag = selectedTags.filter(
+      const duplicateTag = selectedTags.filter(
          (tag) => tag.toLowerCase() === customTag.toLowerCase()
       );
 
-      if (customTag && !isDuplicateTag.length) {
+      if (customTag && !duplicateTag.length) {
          setSelectedTags((prev) => [...prev, customTag]);
          handleClickSgtInput(); // refocus the next available input
          tagRef.current.value = '';
       }
+   };
+
+   const handleDeleteTag = (e, tagName) => {
+      e.stopPropagation();
+
+      setSelectedTags((prev) => prev.filter((tName) => tName !== tagName));
    };
 
    const handleClickSgtInput = () => {
@@ -79,23 +117,78 @@ const Create = () => {
 
    const handleHideSgtBox = () => setShowSgtBox(false);
 
+   //handle rearrange suggestions
+   useEffect(() => {
+      const modifiedSuggestionList = tagSuggestions.filter(
+         (tag) => !selectedTags.includes(tag.tagName)
+      );
+
+      setAvailableTags(modifiedSuggestionList);
+      setSuggestions(modifiedSuggestionList);
+   }, [selectedTags.length]);
+
    //handle click outside
    useEffect(() => {
       if (outsideOfSgtInput && outsideOfSgtBox) {
          handleHideSgtBox();
-         handleSelectCustomTag();
       }
    }, [outsideOfSgtInput, outsideOfSgtBox]);
 
    // dynamic suggestion box position
    useEffect(() => {
       const sgtInput = document.getElementById('sgtInput');
-      const topPx = 52 + sgtInput.offsetHeight;
+      const initialPx = selectedTags.length > 0 ? 76 : 52;
+      const topPx = initialPx + sgtInput.offsetHeight;
       setSgtBoxTopPx(topPx);
    }, [selectedTags.length]);
 
-   const handleSubmit = (e) => {
-      e.preventDefault();
+   // select tags with down | up arrow
+   useEffect(() => {
+      const setIdx = (e) => {
+         const keyCode = e.keyCode;
+
+         if (showSgtBox && selectedTags.length < 3) {
+            if (keyCode === 40) {
+               // keydown
+               if (currentIdx === availableTags.length - 1) {
+                  setCurrentIdx(0);
+               } else {
+                  setCurrentIdx((prev) => prev + 1);
+               }
+
+               setKeyCode(keyCode);
+            } else if (keyCode === 38) {
+               // keyup
+               if (currentIdx === 0 || currentIdx === -1) {
+                  setCurrentIdx(availableTags.length - 1);
+               } else {
+                  setCurrentIdx((prev) => prev - 1);
+               }
+
+               setKeyCode(keyCode);
+            }
+
+            //enter
+            if (keyCode === 13 && currentIdx >= 0) {
+               const selectedTagName = availableTags[currentIdx].tagName;
+               handleSelectTag(selectedTagName);
+            } else if (keyCode === 13) {
+               handleSelectCustomTag();
+            }
+         }
+      };
+
+      window.addEventListener('keydown', setIdx);
+
+      return () => window.removeEventListener('keydown', setIdx);
+   }, [showSgtBox, currentIdx, availableTags.length, selectedTags.length]);
+
+   // clear current idex
+   useEffect(() => {
+      setCurrentIdx(-1);
+   }, [availableTags.length]);
+
+   const handleSubmit = () => {
       const title = titleRef.current.value;
       const tags = selectedTags;
       const body = bodyRef.current.value;
@@ -107,11 +200,7 @@ const Create = () => {
 
    return (
       <div className='max-w-xl w-full mx-auto'>
-         <form
-            action=''
-            className='w-full mt-5 relative'
-            onSubmit={handleSubmit}
-         >
+         <form action='' className='w-full mt-5 relative'>
             <input
                ref={titleRef}
                type='text'
@@ -120,6 +209,11 @@ const Create = () => {
                placeholder='Title'
             />
 
+            {selectedTags.length > 0 && (
+               <small className='text-sm mb-1 text-red-500 block'>
+                  double click to delete tag 🤗
+               </small>
+            )}
             <ul
                id='sgtInput'
                className={`w-full mb-3 ${
@@ -131,8 +225,8 @@ const Create = () => {
                {selectedTags.map((tagName) => (
                   <li
                      key={tagName}
-                     className='h-8 flex items-center text-center bg-tagBg p-2 rounded-md cursor-default'
-                     onClick={(e) => e.stopPropagation()}
+                     className='h-8 flex items-center text-center bg-tagBg p-2 rounded-md cursor-pointer select-none'
+                     onDoubleClick={(e) => handleDeleteTag(e, tagName)}
                   >
                      <p>{tagName}</p>
                   </li>
@@ -143,6 +237,7 @@ const Create = () => {
                      <input
                         ref={tagRef}
                         onChange={handleTagSgt}
+                        onBlur={handleSelectCustomTag}
                         type='text'
                         className={`bg-cardBg w-full rounded-md h-full p-2 ${focusOutlineStyle}`}
                         placeholder='Add up to 3 tags...'
@@ -159,7 +254,11 @@ const Create = () => {
                placeholder='Write your content here...'
             />
 
-            <button className='bg-primary px-5 py-2 rounded-md block mx-auto mt-3 text-white dark:text-zinc-600'>
+            <button
+               type='button'
+               onClick={handleSubmit}
+               className='bg-primary px-5 py-2 rounded-md block mx-auto mt-3 text-white dark:text-zinc-600'
+            >
                Create
             </button>
 
@@ -171,8 +270,11 @@ const Create = () => {
                   className={`bg-cardBg w-full max-w-xl max-h-[320px] overflow-auto rounded-b-md absolute`}
                   style={{ top: `${sgtBoxTopPx}px` }}
                >
-                  {availableTags.map((tag) => (
+                  {availableTags.map((tag, i) => (
                      <SGT
+                        idx={i}
+                        currentIdx={currentIdx}
+                        keyCode={keyCode}
                         {...tag}
                         key={tag.tagName}
                         onClick={handleSelectTag}
